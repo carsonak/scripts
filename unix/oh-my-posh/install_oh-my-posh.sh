dbda6e64680275519c8f88c4fbb29f2cb24a6e18
Andrew <49104171+carsonak@users.noreply.github.com>
2025-06-28 02:46:29 +0300

    Added oh-my-posh

---
+++ unix/oh-my-posh/install_oh-my-posh.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+curl -s https://ohmyposh.dev/install.sh | bash -s
---
+++ unix/oh-my-posh/uninstall_oh-my-posh.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+sudo rm -f "$(which oh-my-posh)"
+echo "Uninstalled oh-my-posh."
+echo "Remove any initialisation lines in the shell rc files."
